346cfb4c0ff2ceece6aff881bf2661a051567017
Michelle <michelledewit1.mdw@gmail.com>
2020-04-19 16:09:11 +0200

    Added router basics

--- vue-lesson-2/src/App.vue
+++ vue-lesson-2/src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link :to="{ name: 'event-list' }">Eventlist</router-link> |
-      <router-link :to="{ name: 'event-show' }">Show event #1</router-link> |
-      <router-link :to="{ name: 'event-create' }">Create</router-link>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
     <router-view />
   </div>
